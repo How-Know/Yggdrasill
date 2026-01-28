8612a4601d8f62da10ac7ee6760f9ae7e422c0de
harry <harry@example.com>
2026-01-28 13:02:26 +0900

    chore(release): bump to 1.0.6.6

--- firmware/m5stack/src/version.h
+++ firmware/m5stack/src/version.h
@@ -3,7 +3,7 @@
 
 // M5Stack 펌웨어 버전
 // GitHub Release 태그(vX.Y.Z.N)와 동기화해야 함 (여기에는 v 없이 X.Y.Z.N만)
-#define FIRMWARE_VERSION "1.0.6.5"
+#define FIRMWARE_VERSION "1.0.6.6"
 #define FIRMWARE_BUILD_DATE __DATE__
 #define FIRMWARE_BUILD_TIME __TIME__
 
